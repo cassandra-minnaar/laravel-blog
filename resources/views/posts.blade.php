08ef10a018850c4a119afc883d10dd6899928619
Cassandra Minnaar <cassandra.minnaar@gmail.com>
2023-02-08 10:41:05 +0200

    Dynamic start - databases

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,19 @@
 <x-layout>
     <x-slot name="posts">
-    @foreach ($posts as $post)
+        @foreach ($posts as $post)
 
-    <article>
+        <article>
 
-        <h1>
-            <a href="/posts/{{$post->slug}}">
-                {{ $post->title; }}
-            </a>
-        </h1>
+            <h1>
+                <a href="/posts/{{$post->id}}">
+                    {{ $post->title; }}
+                </a>
+            </h1>
 
-        <div>
-            {{ $post->excerpt}}
-        </div>
-    </article>
-    @endforeach
+            <div>
+                {{ $post->excerpt}}
+            </div>
+        </article>
+        @endforeach
     </x-slot>
 </x-layout>
