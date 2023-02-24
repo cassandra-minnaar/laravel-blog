7b7f029e5d433b52f6c77847f2f36b84a67bf524
Cassandra Minnaar <cassandra.minnaar@gmail.com>
2023-02-24 12:33:48 +0200

    factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,44 +18,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-
-        $user = User::factory()->create();
-
-        $txt =  Category::create([
-            'name' => 'TXT',
-            'slug' => 'txt'
+       $user = User::factory()->create([
+            'name' => 'NCT 127'
         ]);
 
-        $blackpink = Category::create([
-            'name' => 'Blackpink',
-            'slug' => 'blackpink'
-        ]);
-
-        $bts = Category::create([
-            'name' => 'BTS',
-            'slug' => 'bts'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id ,
-            'category_id' => $txt->id ,
-            'title' => 'Introduction to TXT',
-            'slug' => 'introduction-to-txt',
-            'excerpt' => '<p>Excerpt for txt intro</p>',
-            'body' => '<p>Body for txt with lyrics from BLUE HOUR! But its you! your the one that i want, you everything that i want BLUE</p>'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $blackpink->id, 
-            'title' => 'Introduction to Blackpink',
-            'slug' => 'introduction-to-blackpink', 
-            'excerpt' => '<p>Excerpt for blackpink introduction</p>',
-            'body' => '<p>Blackpink lyrics: I am the typa girl that make you forget that you got a type; Type that make you love me when the only thing you done is like I am that typa girl, I am that typa girl.</p>',
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
         
     }
