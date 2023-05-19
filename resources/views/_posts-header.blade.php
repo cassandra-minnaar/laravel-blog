0ebc21c93ad6dc767a4135aeac3120f43c183815
Cassandra Minnaar <cassandra.minnaar@gmail.com>
2023-05-19 11:39:04 +0200

    Kpop-ified

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -1,13 +1,13 @@
  <header class="max-w-xl mx-auto mt-20 text-center">
      <h1 class="text-4xl">
-         Latest <span class="text-blue-500">Laravel From Scratch</span> News
+         The Latest <span class="text-purple-500">In Boygroups Comeback</span> News
      </h1>
 
      <h2 class="inline-flex mt-2">By Lary Laracore <img src="./images/lary-head.svg" alt="Head of Lary the mascot"></h2>
 
      <p class="text-sm mt-14">
-         Another year. Another update. We're refreshing the popular Laravel series with new content.
-         I'm going to keep you guys up to speed with what's going on!
+         A new year and new comebacks to judge . We're here as a popular blog series to update you on all new content from your favorite boy-groups!
+         I'm here to keep you guys up to speed with what's going on!
      </p>
 
      <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
