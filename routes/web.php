66a683d87234bdafa34960361aaecd0e580841c9
Cassandra Minnaar <cassandra.minnaar@gmail.com>
2023-05-30 14:41:05 +0200

    Complete Dropdown

--- routes/web.php
+++ routes/web.php
@@ -30,25 +30,28 @@ use Symfony\Component\Yaml\Yaml;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->get()
-
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all(),
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {// Post::where('slug', $post)->firstOrFail
     return view('post', [
-        'post' => $post 
+        'post' => $post
     ]);
 });
 
-// Route::get("categories/{category:slug}", function(Category $category) {
-//     return view("posts", [
-//         "posts"=>$category->posts
-//     ]);
-// });
+Route::get("categories/{category:slug}", function(Category $category) {
+    return view("posts", [
+        "posts"=>$category->posts,
+        "currentCategory" => $category,
+        'categories' => Category::all()
+    ]);
+})->name('category');
 
-// Route::get('authors/{author:username}', function(User $author){
-//     return view('posts', [
-//         'posts' => $author->posts
-//     ]);
-// });
+Route::get('authors/{author:username}', function(User $author){
+    return view('posts', [
+        'posts' => $author->posts,
+        'categories' => Category::all(),
+    ]);
+});
